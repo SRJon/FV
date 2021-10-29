f3b6c0934b0376af29731dbdb66cf86c10f7818c
magach07 <jonathanmagacho@gmai.com>
2021-10-29 09:34:31 -0300

    html and css login page

---
+++ front/src/app/Domain/Models/ITela.ts
@@ -0,0 +1,13 @@
+export interface ITela {
+  TelaId:        number | undefined;
+  TelaNome:      string;
+  TelaUrl:       string;
+  TelaAddUrl:    string;
+  TelaTarget:    string;
+  TelaNivel:     boolean;
+  TelaOrdem:     number;
+  TelaModulo:    string;
+  TelaSD:        boolean;
+  TelaImagemSD:  string;
+  TelaIconClass: string;
+}
