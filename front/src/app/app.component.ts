f7e58019c21ae4eca89d0b2c283886b7c743d8dc
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-03 14:56:26 -0300

    middle app component to watch route if is login page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fv';
+
+  isLogin = false;
+
+  constructor(public router: Router) {
+    router.events.pipe().subscribe(() => {
+      this.isLogin = location.pathname.split('/')[1] === 'login';
+    });
+  }
 }
