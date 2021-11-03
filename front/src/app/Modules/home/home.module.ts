e65fa0e0f51bda132c97fd744fe172c90bf041c6
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-03 14:33:27 -0300

    navbar component

--- front/src/app/Modules/home/home.module.ts
+++ front/src/app/Modules/home/home.module.ts
@@ -1,14 +1,18 @@
+import { NavBarComponent } from './Components/NavBar/NavBar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './Components/HomePage/HomePage.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomePageComponent,NavBarComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
   ]
+,
+  bootstrap: [HomePageComponent]
 })
 export class HomeModule { }
