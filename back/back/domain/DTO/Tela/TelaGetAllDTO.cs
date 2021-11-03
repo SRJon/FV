3b49a7e08bbe531445bc01a17a534861aaeff326
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-03 17:04:58 -0300

    telagetALlDTo

---
+++ back/back/domain/DTO/Tela/TelaGetAllDTO.cs
@@ -0,0 +1,17 @@
+namespace back.domain.DTO.Tela
+{
+    public class TelaGetAllDTO
+    {
+        public long TelaId { get; set; }
+        public string TelaNome { get; set; }
+        public string TelaUrl { get; set; }
+        public string TelaAddUrl { get; set; }
+        public string TelaTarget { get; set; }
+        public bool TelaNivel { get; set; }
+        public long TelaOrdem { get; set; }
+        public string TelaModulo { get; set; }
+        public bool TelaSd { get; set; }
+        public string TelaImagemSd { get; set; }
+        public string TelaIconClass { get; set; }
+    }
+}
