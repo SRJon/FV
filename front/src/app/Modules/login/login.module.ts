f47458df76a9d06e9e1b768182661b9ea0a3c8c8
magach07 <jonathanmagacho@gmai.com>
2021-11-11 18:09:20 -0300

    creat email check component

--- front/src/app/Modules/login/login.module.ts
+++ front/src/app/Modules/login/login.module.ts
@@ -6,13 +6,15 @@ import { LoginPageComponent } from './Components/login-page/login-page.component
 import { FormsModule } from '@angular/forms';
 import { LoginMenuComponent } from './Components/login-menu/login-menu.component';
 import { RefactorPasswordComponent } from './Components/refactor-password/refactor-password.component';
+import { EmailCheckComponent } from './Components/email-check/email-check.component';
 
 
 @NgModule({
   declarations: [
     LoginPageComponent,
     LoginMenuComponent,
-    RefactorPasswordComponent
+    RefactorPasswordComponent,
+    EmailCheckComponent
   ],
   imports: [
     CommonModule,
