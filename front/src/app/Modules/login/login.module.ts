9a43319c0af7990fe6a91cdb487e6755595b2fb4
magach07 <jonathanmagacho@gmai.com>
2021-11-11 15:55:38 -0300

    creat refactor password component

--- front/src/app/Modules/login/login.module.ts
+++ front/src/app/Modules/login/login.module.ts
@@ -5,12 +5,14 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './Components/login-page/login-page.component';
 import { FormsModule } from '@angular/forms';
 import { LoginMenuComponent } from './Components/login-menu/login-menu.component';
+import { RefactorPasswordComponent } from './Components/refactor-password/refactor-password.component';
 
 
 @NgModule({
   declarations: [
     LoginPageComponent,
-    LoginMenuComponent
+    LoginMenuComponent,
+    RefactorPasswordComponent
   ],
   imports: [
     CommonModule,
