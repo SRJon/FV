20d31b4a34ba4f94095b4c605f5374dcce9c1255
João Saraiva <joao.saraiva@litoraltextil.com.br>
2021-11-12 17:48:44 -0300

    include - > thenclude

--- back/back/infra/Services/UsuarioServices/UsuarioGetByIdService.cs
+++ back/back/infra/Services/UsuarioServices/UsuarioGetByIdService.cs
@@ -17,7 +17,7 @@ namespace back.infra.Services.UsuarioServices
             //         .Include(w => w.way_nodes.Select(wn => wn.node))
             //         .Load();
             // }
-            var user = await ctx.Usuario.Include(e => e.Perfil.PerfilTela).FirstOrDefaultAsync(x => x.Id == id);
+            var user = await ctx.Usuario.Include(e => e.Perfil).ThenInclude(e => e.PerfilTela).ThenInclude(e => e.Tela).FirstOrDefaultAsync(x => x.Id == id);
 
 
             // user.Perfil= ctx.
