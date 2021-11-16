fae8793088f3ded66a5a43430e096ac5141f46e5
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-16 17:53:24 -0300

    alert

--- front/src/app/Modules/seguranca/Components/tela/Components/tela-grid/tela-grid.component.ts
+++ front/src/app/Modules/seguranca/Components/tela/Components/tela-grid/tela-grid.component.ts
@@ -7,6 +7,7 @@ import {
   Output,
 } from '@angular/core';
 import { ScreensService } from 'src/app/Modules/seguranca/Services/screens.service';
+import { AlertsService } from 'src/app/Repository/Alerts/alerts.service';
 import { ITela } from '../../../../../../Domain/Models/ITela';
 import { Paginate } from '../../../../../../Domain/Models/Paginate';
 
@@ -24,6 +25,22 @@ export class TelaGridComponent implements OnInit, OnChanges {
   selectedRecord: ITela | undefined;
   isDelete: boolean = false;
 
+  constructor(
+    private screensService: ScreensService,
+    private alertsService: AlertsService
+  ) {
+    this.paginate = new Paginate(2000, 50);
+
+    this.titleList = [
+      'id',
+      'nome',
+      'url',
+      'target',
+      'nivel',
+      'ordem',
+      'modulo',
+    ];
+  }
   clickOnPagination(page: number): void {
     this.nextSelection.emit(page);
   }
@@ -42,30 +59,42 @@ export class TelaGridComponent implements OnInit, OnChanges {
     }
   }
   async onDelete(obj: ITela | undefined = undefined): Promise<void> {
-    if (obj) {
-      this.screensService
-        .deleteScreen(obj.id || 0)
-        .then((res) => {
-          console.log(res, 'res');
-        })
-        .catch((err) => {
-          console.log(err, 'ress');
-        });
-    }
+    this.selectedRecord = obj;
+
+    this.isDelete = true;
   }
+  delete(id: number) {
+    this.screensService
+      .deleteScreen(id || 0)
+      .then((res) => {
+        this.clickOnPagination(this.paginate.currentPage);
+        console.log(res, 'res');
 
-  constructor(private screensService: ScreensService) {
-    this.paginate = new Paginate(2000, 50);
-
-    this.titleList = [
-      'id',
-      'nome',
-      'url',
-      'target',
-      'nivel',
-      'ordem',
-      'modulo',
-    ];
+        if (res.data) {
+          this.alertsService.showAlert('Tela excluida com sucesso', 'success');
+        } else {
+          this.alertsService.showAlert('Erro ao excluir tela', 'error');
+        }
+      })
+      .catch((err) => {
+        this.alertsService.showAlert(
+          `Erro ao excluir tela --- ${err.message}`,
+          'error'
+        );
+      })
+      .finally(() => {
+        this.isDelete = false;
+      });
+  }
+  onModalClose(isClose: boolean) {
+    if (isClose) {
+      this.selectedRecord = undefined;
+      this.isDelete = false;
+    } else {
+      if (this.selectedRecord) {
+        this.delete(this.selectedRecord.id || 0);
+      }
+    }
   }
 
   ngOnInit(): void {
@@ -81,6 +110,9 @@ export class TelaGridComponent implements OnInit, OnChanges {
         paging: false,
         lengthChange: false,
         info: '',
+        language: {
+          zeroRecords: ' ',
+        },
       });
 
       ctx.setPaginate(ctx);
--- front/src/app/Modules/seguranca/Services/Actions/ScreenDelete.ts
+++ front/src/app/Modules/seguranca/Services/Actions/ScreenDelete.ts
@@ -3,6 +3,6 @@ import { IResponse } from '../../../../Domain/Models/IResponse';
 
 export async function Delete(id: number): Promise<IResponse<boolean>> {
   return await (
-    await axios.post(`/api/Tela/Delete?id${id}`)
+    await axios.post(`/api/Tela/Delete?id=${id}`)
   ).data;
 }
