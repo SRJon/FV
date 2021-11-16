cff0639a4d392a8f0f15f5153608515fd6e87320
João Saraiva <joao.saraiva@litoraltextil.com.br>
2021-11-16 16:16:46 -0300

    Correção relação tela - tela Ajuste Create Tela

--- back/back/domain/DTO/ScreenDTO/TelaDTOCreate.cs
+++ back/back/domain/DTO/ScreenDTO/TelaDTOCreate.cs
@@ -1,26 +1,19 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using back.domain.entities;
 
-namespace back.data.entities.Screen
+namespace back.domain.DTO.ScreenDTO
 {
-    public class Tela : ITela
+    public class TelaDTOCreate : ITela
     {
-        public int Id { get; set; }
         public string Nome { get; set; }
         public string Url { get; set; }
         public string AddUrl { get; set; }
         public string Target { get; set; }
         public bool Nivel { get; set; }
-        public Int16 Ordem { get; set; }
+        public short Ordem { get; set; }
         public string Modulo { get; set; }
         public bool Sd { get; set; }
         public string ImagemSd { get; set; }
         public string IconClass { get; set; }
-        public int? TelaId { get; set; }
-
-        public virtual Tela? tela { get; set; } = null;
-
+        public int? TelaId { get; set; } = null;
     }
 }
--- back/back/infra/Services/TelaServices/ScreenCreateService.cs
+++ back/back/infra/Services/TelaServices/ScreenCreateService.cs
@@ -1,14 +1,19 @@
+using AutoMapper;
 using back.data.entities.Screen;
+using back.domain.DTO.ScreenDTO;
 using back.infra.Data.Context;
+using back.MappingConfig;
 using System.Threading.Tasks;
 
 namespace back.infra.Services.TelaServices
 {
     public static class ScreenCreateService
     {
-        public static Task<bool> Create(this DbAppContextFVUDB_TESTE ctx, Tela tela)
+        private static readonly IMapper _mapper = MapperConfig.MapperConfiguration().CreateMapper();
+        
+        public static Task<bool> Create(this DbAppContextFVUDB_TESTE ctx, TelaDTOCreate tela)
         {
-            ctx.Tela.Add(tela);
+            ctx.Tela.Add(_mapper.Map<Tela>(tela));
             var result = ctx.SaveChanges();
             return result > 0 ? Task.FromResult(true) : Task.FromResult(false);
         }
