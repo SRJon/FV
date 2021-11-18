d264fcca13dfcebb59807762cf3e14687266f537
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-18 14:35:37 -0300

    ScreenCreate

---
+++ front/src/app/Modules/seguranca/Services/Actions/ScreenCreate.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { IResponse } from 'src/app/Domain/Models/IResponse';
+import { ITela } from '../../../../Domain/Models/ITela';
+
+export async function ScreenCreate(screen: ITela): Promise<IResponse<boolean>> {
+  let response = await axios.post('/api/Tela/Create', screen);
+  console.log(response);
+  return response.data;
+}
--- front/src/app/Modules/seguranca/Services/Actions/index.ts
+++ front/src/app/Modules/seguranca/Services/Actions/index.ts
@@ -1,5 +1,6 @@
 import { GetAll } from './ScreenGetAll';
 import { Delete } from './ScreenDelete';
 import { ScreenUpdate } from './ScreenUpdate';
+import { ScreenCreate } from './ScreenCreate';
 
-export { GetAll, Delete, ScreenUpdate };
+export { GetAll, Delete, ScreenUpdate, ScreenCreate };
