5a902c27f03ecb47a08ad20c70ff42a89264e3c4
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-24 14:31:29 -0300

    perfil telas

--- front/src/app/Domain/Models/IPerfil.ts
+++ front/src/app/Domain/Models/IPerfil.ts
@@ -1,5 +1,9 @@
+import { IPerfilTela } from './IPerfilTela';
+import { IUser } from './IUser';
 export interface IPerfil {
   id: number;
   nome: string;
   peR_COD: number;
+  perfilTela?: IPerfilTela[];
+  usuario?: IUser[];
 }
