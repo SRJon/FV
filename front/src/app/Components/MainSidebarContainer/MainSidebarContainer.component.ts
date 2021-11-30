b2cd9800695ad8c65b30e83a1a142361d13a6ee7
leonardoigor <igor_mendonca@outlook.com.br>
2021-11-30 12:22:04 -0300

    fix bug on screen

--- front/src/app/Components/MainSidebarContainer/MainSidebarContainer.component.ts
+++ front/src/app/Components/MainSidebarContainer/MainSidebarContainer.component.ts
@@ -42,7 +42,6 @@ export class MainSidebarContainerComponent implements OnInit {
 
       granScreens.forEach((e, i) => {
         let related = subScreens.filter((sub) => sub.telaId == e.id);
-        console.log(related, e.id);
 
         related.sort((a, b) => (b.ordem > a.ordem ? -1 : 1));
         // console.log(related.map((e) => e.ordem));
