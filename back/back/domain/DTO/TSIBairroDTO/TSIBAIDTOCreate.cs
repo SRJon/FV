77d1b51d8a1b96c99df6db1a8abb38f77f0ad575
João Saraiva <joao.saraiva@litoraltextil.com.br>
2021-11-30 15:04:21 -0300

    inclusão repositórios TSIbairro

---
+++ back/back/domain/DTO/TSIBairroDTO/TSIBAIDTOCreate.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using back.domain.entities;
+
+namespace back.domain.DTO.TSIBairroDTO
+{
+    public class TSIBAIDTOCreate : ITSIBAI
+    {
+        public string NomeBai { get; set; }
+        public int Codreg { get; set; }
+        public DateTime Dtalter { get; set; }
+        public string DescricaoCorreio { get; set; }
+        public int? Nuversao { get; set; }
+    }
+}
