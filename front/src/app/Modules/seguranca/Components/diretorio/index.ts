6c8651f461a09d96ec1729b73cc6d69beecee02b
leonardoigor <igor_mendonca@outlook.com.br>
2021-12-09 16:32:47 -0300

    refactore

---
+++ front/src/app/Modules/seguranca/Components/diretorio/index.ts
@@ -0,0 +1,11 @@
+import { DiretorioContentComponent } from './Components/diretorio-content/diretorio-content.component';
+import { EditDiretorioComponentComponent } from './Components/diretorio-content/edit-diretorio-component/edit-diretorio-component.component';
+import { DiretorioGridComponent } from './Components/diretorio-grid/diretorio-grid.component';
+import { DiretorioComponent } from './diretorio.component';
+
+export {
+  DiretorioContentComponent,
+  DiretorioComponent,
+  DiretorioGridComponent,
+  EditDiretorioComponentComponent,
+};
---
+++ front/src/app/Modules/seguranca/Components/usuario/index.ts
@@ -0,0 +1,4 @@
+import { UsuarioEditModalComponent } from './Components/usuario-edit-modal/usuario-edit-modal.component';
+import { UsuarioComponent } from './usuario.component';
+
+export { UsuarioComponent, UsuarioEditModalComponent };
