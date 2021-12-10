3976f5b353bc72961a7bc28e00eef8f41b9319ee
Fotus Energia <wans05@msn.com>
2021-12-10 09:37:33 -0300

    Corrections Merge Navbar

--- front/src/app/Components/NavBar/NavBar.component.ts
+++ front/src/app/Components/NavBar/NavBar.component.ts
@@ -8,20 +8,23 @@ import { AsideMenu } from 'src/app/Models/AsideMenu';
 import { GlobalMenuService } from 'src/app/Shared/global-menu.service';
 import { GlobalTitle } from 'src/app/Shared/GlobalTitle';
 
-
 @Component({
   selector: 'app-NavBar',
   templateUrl: './NavBar.component.html',
   styleUrls: ['./NavBar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  empresas: IEmpresa[] = [];
 
-  empresas: IEmpresa[] = [
+  isOpen = false;
+  aside = new AsideMenu();
+  _title = '';
 
-  ];
-
-  constructor(private userG: UserGlobal<IUser>) {}
-
+  constructor(
+    private menuAsideObs: GlobalMenuService<AsideMenu>,
+    private globalTitle: GlobalTitle<string>,
+    private userG: UserGlobal<IUser>
+  ) {}
   ngOnInit() {
     this.userG.getObservable().subscribe((user) => {
       let emp = user.usuarioEmp;
@@ -33,24 +36,14 @@ export class NavBarComponent implements OnInit {
         this.empresas = nonNUll;
       }
     });
-  isOpen = false;
-  aside = new AsideMenu();
-  _title = '';
-  constructor(
-    private menuAsideObs: GlobalMenuService<AsideMenu>,
-    private globalTitle: GlobalTitle<string>
-  ) {}
 
-  ngOnInit() {
     this.globalTitle.getObservable().subscribe((value) => {
       this._title = value;
     });
   }
-
   onMenuClickModal() {
     this.isOpen = !this.isOpen;
     this.aside.setValue(this.isOpen);
     this.menuAsideObs.set(this.aside);
-
   }
 }
