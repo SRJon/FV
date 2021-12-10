c673e94a21bb3db9b414d82c0770218bcb7508ab
leonardoigor <igor_mendonca@outlook.com.br>
2021-12-10 17:04:27 -0300

    grids

--- front/src/app/Modules/representante/representante.module.ts
+++ front/src/app/Modules/representante/representante.module.ts
@@ -7,6 +7,15 @@ import { ClientGridComponent } from './Components/client/Components/client-grid/
 import { Min_loadingComponent } from 'src/app/Components/min_loading/min_loading/min_loading.component';
 import { GeralComponent } from './Components/client/Components/client_details/Components/Geral/Geral.component';
 import { Client_detailsComponent } from './Components/client/Components/client_details/client_details.component';
+import { ClientPerfilComponent } from './Components/client/Components/client_details/Components/client-perfil/client-perfil.component';
+import { GridClientPerfilComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/grid-client-perfil/grid-client-perfil.component';
+import { SolicitacaoDevolucaoComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/solicitacao-devolucao/solicitacao-devolucao.component';
+import { DinamicFilterComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/dinamic-filter/dinamic-filter.component';
+import { GridSolicitacaoDevolucaoComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/grid-solicitacao-devolucao/grid-solicitacao-devolucao.component';
+import { GridNotaFiscalComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/grid-nota-fiscal/grid-nota-fiscal.component';
+import { NotaFiscalComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/nota-fiscal/nota-fiscal.component';
+import { FinanceiroGridComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/financeiro-grid/financeiro-grid.component';
+import { FinanceiroComponent } from './Components/client/Components/client_details/Components/client-perfil/Components/financeiro/financeiro.component';
 
 @NgModule({
   imports: [CommonModule, RepresentanteRoutingModule, SegurancaModule],
@@ -16,6 +25,15 @@ import { Client_detailsComponent } from './Components/client/Components/client_d
     ClientGridComponent,
     Min_loadingComponent,
     Client_detailsComponent,
+    ClientPerfilComponent,
+    GridClientPerfilComponent,
+    SolicitacaoDevolucaoComponent,
+    DinamicFilterComponent,
+    GridSolicitacaoDevolucaoComponent,
+    GridNotaFiscalComponent,
+    NotaFiscalComponent,
+    FinanceiroGridComponent,
+    FinanceiroComponent,
   ],
   bootstrap: [ClientComponent],
   exports: [ClientComponent, GeralComponent],
