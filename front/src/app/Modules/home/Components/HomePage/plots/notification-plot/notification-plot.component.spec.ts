c7f262ff2209f30a040c6b66d2618dd54a1545ea
leonardoigor <igor_mendonca@outlook.com.br>
2021-12-13 16:38:26 -0300

    plots

---
+++ front/src/app/Modules/home/Components/HomePage/plots/notification-plot/notification-plot.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotificationPlotComponent } from './notification-plot.component';
+
+describe('NotificationPlotComponent', () => {
+  let component: NotificationPlotComponent;
+  let fixture: ComponentFixture<NotificationPlotComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NotificationPlotComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotificationPlotComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front/src/app/Modules/home/Components/HomePage/plots/sales-plot/sales-plot.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SalesPlotComponent } from './sales-plot.component';
+
+describe('SalesPlotComponent', () => {
+  let component: SalesPlotComponent;
+  let fixture: ComponentFixture<SalesPlotComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SalesPlotComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SalesPlotComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
