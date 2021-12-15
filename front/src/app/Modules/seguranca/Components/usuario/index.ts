3cad73748b5bf5d216e7ab59363110a3373de80f
leonardoigor <igor_mendonca@outlook.com.br>
2021-12-15 16:03:42 -0300

    pull

--- front/src/app/Modules/seguranca/Components/usuario/index.ts
+++ front/src/app/Modules/seguranca/Components/usuario/index.ts
@@ -1,4 +1,11 @@
-import { UsuarioEditModalComponent } from './Components/usuario-edit-modal/usuario-edit-modal.component';
 import { UsuarioComponent } from './usuario.component';
+import { EditUsuarioComponentComponent } from './Components/usuario-content/edit-usuario-component/edit-usuario-component.component';
+import { UsuarioContentComponent } from './Components/usuario-content/usuario-content.component';
+import { UsuarioGridComponent } from './Components/usuario-grid/usuario-grid.component';
 
-export { UsuarioComponent, UsuarioEditModalComponent };
+export default [
+  UsuarioComponent,
+  EditUsuarioComponentComponent,
+  UsuarioContentComponent,
+  UsuarioGridComponent,
+];
--- front/src/app/Modules/seguranca/seguranca.module.ts
+++ front/src/app/Modules/seguranca/seguranca.module.ts
@@ -4,22 +4,6 @@ import { SegurancaRoutingModule } from './seguranca-routing.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { EditTelaComponentComponent } from './Components/tela/Components/tela-content/edit-tela-component/edit-tela-component.component';
-import { EmpresaGridComponent } from './Components/empresa/Components/empresa-grid/empresa-grid.component';
-import { EditEmpresaComponentComponent } from './Components/empresa/Components/empresa-content/edit-empresa-component/edit-empresa-component.component';
-import { EmpresaContentComponent } from './Components/empresa/Components/empresa-content/empresa-content.component';
-import { ParametroGridComponent } from './Components/parametro/Components/parametro-grid/parametro-grid.component';
-import { ParametroContentComponent } from './Components/parametro/Components/parametro-content/parametro-content.component';
-import { EditParametroComponentComponent } from './Components/parametro/Components/parametro-content/edit-parametro-component/edit-parametro-component.component';
-import { DiretorioContentComponent } from './Components/diretorio/Components/diretorio-content/diretorio-content.component';
-import { DiretorioGridComponent } from './Components/diretorio/Components/diretorio-grid/diretorio-grid.component';
-import { EditDiretorioComponentComponent } from './Components/diretorio/Components/diretorio-content/edit-diretorio-component/edit-diretorio-component.component';
-import { PerfilGridComponent } from './Components/perfil/Components/perfil-grid/perfil-grid.component';
-import { UsuarioGridComponent } from './Components/usuario/Components/usuario-grid/usuario-grid.component';
-import { UsuarioContentComponent } from './Components/usuario/Components/usuario-content/usuario-content.component';
-import { EditUsuarioComponentComponent } from './Components/usuario/Components/usuario-content/edit-usuario-component/edit-usuario-component.component';
-
-
 import * as perfil from './Components/perfil';
 import * as tela from './Components/tela';
 import * as diretorio from './Components/diretorio';
@@ -35,31 +19,6 @@ import * as user from './Components/usuario';
     ReactiveFormsModule,
   ],
   declarations: [
-
-    PerfilComponent,
-    TelaComponent,
-    EmpresaComponent,
-    UsuarioComponent,
-    DiretorioComponent,
-    ParametroComponent,
-    TitleComponent,
-    TelaContentComponent,
-    TelaGridComponent,
-    EditTelaComponentComponent,
-    EmpresaGridComponent,
-    EditEmpresaComponentComponent,
-    EmpresaContentComponent,
-    ParametroGridComponent,
-    ParametroContentComponent,
-    EditParametroComponentComponent,
-    DiretorioContentComponent,
-    DiretorioGridComponent,
-    EditDiretorioComponentComponent,
-    PerfilGridComponent,
-    UsuarioGridComponent,
-    UsuarioContentComponent,
-    EditUsuarioComponentComponent,
-
     perfil.PerfilComponent,
     perfil.PerfilGridComponent,
     tela.TelaComponent,
@@ -67,8 +26,10 @@ import * as user from './Components/usuario';
     tela.TelaContentComponent,
     tela.TelaGridComponent,
     tela.EditTelaComponentComponent,
-    user.UsuarioComponent,
-    user.UsuarioEditModalComponent,
+    // user.UsuarioComponent,
+    // user.EditUsuarioComponentComponent,
+    // user.UsuarioContentComponent,
+    ...user.default,
     emp.EmpresaComponent,
     emp.EmpresaGridComponent,
     emp.EditEmpresaComponentComponent,
@@ -81,7 +42,6 @@ import * as user from './Components/usuario';
     diretorio.DiretorioContentComponent,
     diretorio.DiretorioGridComponent,
     diretorio.EditDiretorioComponentComponent,
-
   ],
   bootstrap: [perfil.PerfilComponent],
   exports: [tela.TitleComponent],
