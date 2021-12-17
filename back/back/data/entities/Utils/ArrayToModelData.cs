af2db826ee6a3c544b688e42a10e635a433248c4
leonardoigor <igor_mendonca@outlook.com.br>
2021-12-17 15:16:51 -0300

    utils

---
+++ back/back/data/entities/Utils/ArrayToModelData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace back.data.entities.Utils
+{
+    public class ArrayToModelData
+    {
+        public string model { get; set; }
+        public List<string> data { get; set; }
+    }
+}
