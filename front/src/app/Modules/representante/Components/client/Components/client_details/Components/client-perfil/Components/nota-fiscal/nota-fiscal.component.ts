0b3a5ca9f5ef9e7ec90be909974877d5b80dd94b
leonardoigor <igor_mendonca@outlook.com.br>
2022-01-03 08:43:36 -0300

    scripts

--- front/src/app/Modules/representante/Components/client/Components/client_details/Components/client-perfil/Components/nota-fiscal/nota-fiscal.component.ts
+++ front/src/app/Modules/representante/Components/client/Components/client_details/Components/client-perfil/Components/nota-fiscal/nota-fiscal.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nota-fiscal',
   templateUrl: './nota-fiscal.component.html',
-  styleUrls: ['./nota-fiscal.component.scss']
+  styleUrls: ['./nota-fiscal.component.scss'],
 })
 export class NotaFiscalComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
