fe827b74879c9ecdfc857ec110b873728bf84dd4
leonardoigor <igor_mendonca@outlook.com.br>
2022-01-04 16:03:13 -0300

    types fix

--- front/src/app/Modules/representante/Components/client/Components/client_details/Components/client-perfil/Components/solicitacao-devolucao/solicitacao-devolucao.component.ts
+++ front/src/app/Modules/representante/Components/client/Components/client_details/Components/client-perfil/Components/solicitacao-devolucao/solicitacao-devolucao.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ISolicitacaoDevolucao } from 'src/app/Domain/Models/ISolicitacaoDevolucao';
 import { ADVGFRPVService } from 'src/app/Modules/representante/Services/AD_VGFRPV/ad-vgfrpv.service';
 
 @Component({
@@ -9,11 +10,14 @@ import { ADVGFRPVService } from 'src/app/Modules/representante/Services/AD_VGFRP
 })
 export class SolicitacaoDevolucaoComponent implements OnInit {
   id: string = '';
+  listGrid: ISolicitacaoDevolucao[];
+  contentHeight: number = 0;
 
   constructor(
     private service: ADVGFRPVService,
     private _Activatedroute: ActivatedRoute
   ) {
+    this.listGrid = [];
     this._Activatedroute.paramMap.subscribe((params) => {
       this.id = params.get('id') as string;
     });
@@ -21,6 +25,7 @@ export class SolicitacaoDevolucaoComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAll(Number(this.id));
+    this.getSize();
   }
 
   getAll(codparc: number, page: number = 1, limit: number = 10) {
@@ -28,7 +33,20 @@ export class SolicitacaoDevolucaoComponent implements OnInit {
       .GetSolicitacaoDevolucao(codparc, page, limit)
       .then((response) => {
         // this.data.parceiro = response.data;
-        console.log(response.data);
+        this.listGrid = response.data.sort((a, b) => b.nusoldev - a.nusoldev);
       });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getSize() {
+    let doc = document.querySelector('#tab_4');
+    let contentHeight = doc?.clientHeight;
+
+    this.contentHeight = contentHeight || 0;
+    this.contentHeight -= 100;
+    this.contentHeight = this.contentHeight < 0 ? 0 : this.contentHeight;
+    if (document.body.clientWidth <= 768) {
+      this.contentHeight -= 30;
+    }
   }
 }
